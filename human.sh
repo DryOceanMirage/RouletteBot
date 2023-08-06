31d46d35775babb550ab0b1709fb753ed2f4b64d
DryOceanMirage <david.pasko.work@gmail.com>
2023-08-06 16:02:10 +1000

    new human game

--- human.sh
+++ human.sh
@@ -1,18 +1,34 @@
 #!/bin/bash
 
-source ./start.sh
-source ./spin.sh
 
+human_game() {
 read -p "Would you like to open a saved game(Yes/no)? " savegame
 
 if [[ "$savegame" == "no" || "$savegame" == "n" ]]; then
 	clear
+	new_human_game
 	
 else
 	clear
-	read -p "Please enter savename: " savefile
+	read -p "Please enter savename(Case sensitive): " savefile
 	while IFS= read -r line; do
 		eval "$line"
 	done < $savegame
 fi
+}
+#Creates a new game
+new_human_game() {
+clear
+echo "Welcome to the game of roulette"
+read -p "Please enter starting bankroll: " hbankroll
+hwin=0
+hloss=0
+hmoneywon=0
+hmoneyloss=0
+hplay
+}
 
+hplay() {
+clear
+read -p "Place your bets: " hbet
+}
