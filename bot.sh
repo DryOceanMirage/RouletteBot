bd9d560af3ba7f733a87af16ba26d3a8a3e94bd1
DryOceanMirage <david.pasko.work@gmail.com>
2023-08-22 14:38:06 +1000

    playbotintroduced

--- bot.sh
+++ bot.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+bnumplay=0
+bwin=0
+bloss=0
+bmoneywon=0
+bmoneyloss=0
 
 #is there a savefile avalible
 bot_game() {
@@ -17,17 +22,45 @@ fi
 
 setparameters() {
 clear
+
 echo "Please set up your parameters:"
 read -p "Minimum bet: " bbet
 read -p "Should the minimum bet be doubled on a loss? " doubleonloss
 read -p "Total bankroll: " bbankroll
 read -p "Betting colour: " bcolor
 read -p "Should the bot stop if bankroll reaches zero? " bbankrollzero
-read -p "Number of games that should be played: " bnumplay
+read -p "Number of games that should be played: " btotalplay
 playbot
 }
 
 playbot() {
+clear
+spin_wheel
+if [[ "$color" == "$bcolor" ]]; then
+        hwin=$((bwin + 1))
+        bmoneywon=$((bmoneywon + bet_amount))
+        hbankroll=$((bbankroll + bet_amount))
+        echo "Congratulations! You won $bet_amount"
+else
+        bloss=$((bloss + 1))
+        bmoneyloss=$((bmoneyloss + bet_amount))
+        bbankroll=$((bbankroll - bet_amount))
+        echo "Sorry, you lost $bet_amount"
+fi
+echo "Current balance: $bbankroll"
+echo "Playing game $bnumplay out of $btotalplay"
+bnumplay=$((bnumplay+1))
+
+}
+checkbot() {
+if [[ "$bnumplay" != "$btotalplay" ]]; then
+playbot
+
+else
+summary
 
 }
 
+summary(){
+
+}
